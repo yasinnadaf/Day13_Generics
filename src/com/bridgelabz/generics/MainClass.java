5f9d4938166f7f1a9804210fdd61a2fb7e24e160
yasin nadaf <yasin.nadaf7414@gmai.com>
2022-08-26 21:56:52 +0530

    Modified: max string value

--- src/com/bridgelabz/generics/MainClass.java
+++ src/com/bridgelabz/generics/MainClass.java
@@ -3,12 +3,12 @@ package com.bridgelabz.generics;
 public class MainClass {
     public static void main(String[] args) {
 
-        MaxFloat maxFloat  = new MaxFloat();
+        MaxString maxString  = new MaxString();
         System.out.println("Test case 1");
-        maxFloat.maxValue(1.8F,4.6F,2.9F);
+        maxString.maxValue("Apple","Peach","Banana");
         System.out.println("Test case 2");
-        maxFloat.maxValue(5.1F,4.5F,6.8F);
+        maxString.maxValue("Apple","Zach","Banana");
         System.out.println("Test case 3");
-        maxFloat.maxValue(7.6F,10.0F,8.3F);
+        maxString.maxValue("Apple","Banana","Class");
     }
 }
