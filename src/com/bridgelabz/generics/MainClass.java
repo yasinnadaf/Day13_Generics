369349e602b4915344c58dc738d9168add3b0c5a
yasin nadaf <yasin.nadaf7414@gmai.com>
2022-08-28 22:53:31 +0530

    modified: using generic class

--- src/com/bridgelabz/generics/MainClass.java
+++ src/com/bridgelabz/generics/MainClass.java
@@ -3,24 +3,12 @@ package com.bridgelabz.generics;
 public class MainClass {
     public static void main(String[] args) {
 
-        MaxValue maxValue = new MaxValue();
-        System.out.println("Test case 1");
-        maxValue.maxValue(1,3,2);
-        System.out.println("Test case 2");
-        maxValue.maxValue(5,4,6);
-        System.out.println("Test case 3");
-        maxValue.maxValue(7,10,8);
-        System.out.println("Test case 1");
-        maxValue.maxValue(1.8F,4.6F,2.9F);
-        System.out.println("Test case 2");
-        maxValue.maxValue(5.1F,4.5F,6.8F);
-        System.out.println("Test case 3");
-        maxValue.maxValue(7.6F,10.0F,8.3F);
-        System.out.println("Test case 1");
-        maxValue.maxValue("Apple","Peach","Banana");
-        System.out.println("Test case 2");
-        maxValue.maxValue("Apple","Zach","Banana");
-        System.out.println("Test case 3");
-        maxValue.maxValue("Apple","Banana","Class");
+        MaxValue<Integer> maxValue = new MaxValue<>(5,4,3);
+        maxValue.testMaximum();
+        MaxValue<Float> maxValue1 = new MaxValue<>(5.2F,8.3F,3.2F);
+        maxValue1.testMaximum();
+        MaxValue<String> maxValue2 = new MaxValue<>("Peach","Apple","Zach");
+        maxValue2.testMaximum();
     }
+
 }
--- src/com/bridgelabz/generics/MaxValue.java
+++ src/com/bridgelabz/generics/MaxValue.java
@@ -1,17 +1,28 @@
 package com.bridgelabz.generics;
 
-public class MaxValue {
-    public <K extends Comparable <K>> void maxValue(K val1, K val2, K val3){
-            if((val1.compareTo(val2)) >=0 && (val1.compareTo(val3)) >=0)
-                System.out.println(val1+ " is the largest: ");
+    public class MaxValue<K extends Comparable<K>> {
 
-            else if ((val2.compareTo(val1)) >=0 && (val2.compareTo(val3)) >=0) {
-                System.out.println(val2+" is largest: ");
+        K var1,var2,var3;
+
+        public MaxValue(K var1, K var2, K var3) {
+            this.var1 = var1;
+            this.var2 = var2;
+            this.var3 = var3;
+        }
+
+
+        public void testMaximum(){
+            testMaximum(var1,var2,var3);
+        }
+
+        static <T extends Comparable<T>> void testMaximum(T val1,T val2,T val3){
+            if((val1.compareTo(val2))>=0 && (val1.compareTo(val3))>=0)
+                System.out.println(val1+ " is the largest");
+            else if ((val2.compareTo(val1))>=0 && (val2.compareTo(val3))>=0) {
+                System.out.println(val2+" is the largest");
             }
             else{
-                System.out.println(val3+" is largest: ");
-
+                System.out.println(val3+" is the largest");
             }
-
-    }
+        }
 }
